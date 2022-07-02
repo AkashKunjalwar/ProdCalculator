b77c27c9d4b3f75d4d9f887c5a97963103c7168b
Akash Kunjalwar <akash.kunjalwar@globallogic.com>
2022-07-02 22:20:53 +0530

    initial app commit-2

--- src/Components/Output/About/About.js
+++ src/Components/Output/About/About.js
@@ -6,14 +6,14 @@ const About = () => {
     <div className={styles.aboutSection}>
       <h3>About Us</h3>
       <p>
-        This is the first version of the application "KNOW YOUR PROD". There are
-        yet many changes that are required to make this application work in any
-        device. However, it is to be noted that this is a web application. As
-        mentioned earlier, the application is being developed to be able to
+        v1.0.0 is the latest version of the application "KNOW YOUR PROD". There
+        are yet many changes that are required to make this application work in
+        any device. However, it is to be noted that this is a web application.
+        As mentioned earlier, the application is being developed to be able to
         support mobile devices or any other non-desktop devices. The contents in
         the application might distort based on browser zooming. The application
-        works in an expected manner when the browser zoom is in between 70% to
-        130% with best at 100% and the width of the screen is between 760px to
+        works in an expected manner when the browser zoom is in between 70% and
+        130% with best at 100% and the width of the screen is between 760px and
         1280px.
       </p>
     </div>
